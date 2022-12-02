61183b8be790b950b6522b23bcd4e3245b7082d4
Ty <justfacey@gmail.com>
2022-12-02 15:09:03 -0800

    added button to allow users to add new tasks

--- src/Column.tsx
+++ src/Column.tsx
@@ -1,3 +1,4 @@
+import { AddNewItem } from "./AddNewItem"
 import { Card } from "./Card"
 import { ColumnContainer, ColumnTitle } from "./styles"
 
@@ -14,6 +15,11 @@ export const Column = ({ text }: ColumnProps) => {
             <Card text="Build" />
             <Card text="Research & Test" />
             <Card text="Build some more" />
+            <AddNewItem
+                toggleButtonText="+ Add another list"
+                onAdd={() => console.log("Item created")}
+                dark
+            />
       </ColumnContainer>
     )
 }
